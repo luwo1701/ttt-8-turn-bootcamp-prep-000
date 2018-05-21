62138851958878960a0c09898d73dadf76e9ecdf
Luke Worley <luke.worley@colorado.edu>
2018-05-21 18:24:19 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,15 +1,17 @@
 def turn(board)
-  puts "Please enter 1-9:"
+  moved = false
   
-  input = gets.strip()
-  index = input_to_index(input)
-  #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  if valid_move?(board,index)
-    move(board,index)
-    return display_board(board)
-  else
-    puts "Please enter 1-9"
-  end
+  while moved==false
+    puts "Please enter 1-9:"
+    input = gets.strip()
+    index = input_to_index(input)
+    #board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+    if valid_move?(board,index)
+      move(board,index)
+      return display_board(board)
+    else
+      puts "Please enter 1-9"
+    end
 end
 
 def display_board(board)
